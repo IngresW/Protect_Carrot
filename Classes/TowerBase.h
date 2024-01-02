184a5870902f82ec71c681605003fe9e331c3ff2
JOYLEN-SHEN <140143381+JOYLEN-SHEN@users.noreply.github.com>
2024-01-02 22:42:28 +0800

    Update TowerBase.h

--- Classes/TowerBase.h
+++ Classes/TowerBase.h
@@ -8,33 +8,29 @@ class EntityAffected;
 class TowerBase :public Entity
 {
 public:
-    virtual int getTowerUpGradeCost();
-    virtual void showGradeMark();
-    virtual void hideGradeMark();
+    virtual bool init(const int& rId, const BasicInformation basicInformation, const TowerInformation towerInformation);
     virtual bool isInAtkRange(const Vec2& rPOS);
-    virtual void setAtkTarget(EntityAffected* pEntityAffected);
+    virtual void checkAtkTarget(float dt);
+    virtual void fire(float dt);
     virtual int getTowerGradeCost();
 
-protected:
-    virtual bool init(const int& rId);
-    virtual void initTower();
-    virtual void upGrade();
-    virtual void loadTowerData();
-    virtual void fireAction();
+    BulletInformation _iBulletId;
+    double _iAtkRange;
+    int _iCreateCost;
+    int _iUpgradeCost;
+    int _iValue;
+    int _iGrade;
+    int _iTowerId;
     
+    bool _bAtkTarget;
 
-    CC_SYNTHESIZE_PASS_BY_REF(int, _iBulletId, IBulletId);
-    CC_SYNTHESIZE_PASS_BY_REF(int, _iAtkRange, IAtkRange);
-    CC_SYNTHESIZE_PASS_BY_REF(double, _iAtkSpace, IAtkSpace);
-    CC_SYNTHESIZE_PASS_BY_REF(std::string, _sUpgradeCost, SUpgradeCose);
-    CC_SYNTHESIZE_PASS_BY_REF(int, _iUpgradeCount, IUpgradeCount);
-    CC_SYNTHESIZE_PASS_BY_REF(bool, _bIsRotation, BIsRotation);
-    CC_SYNTHESIZE_PASS_BY_REF(int, _iCreateCost, ICreateCost);
-    CC_SYNTHESIZE_PASS_BY_REF(bool, _bIsHaveAtkTarget, IsHaveAtkTarget);
+protected:
+    virtual void upGrade();
 
-    Sprite* _pGradeMark = nullptr;
-    Sprite* _pTowerPanel = nullptr;
+
+    Sprite* _pTowerSprite = nullptr;
     EntityAffected* _pAtkTarget = nullptr;
+    std::vector<MonsterBase*> MonsterVec;
 };
 
 #endif
