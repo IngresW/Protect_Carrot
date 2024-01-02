fbcda90e5f91cc67a273719277dd38a71944a71f
JOYLEN-SHEN <140143381+JOYLEN-SHEN@users.noreply.github.com>
2024-01-02 22:39:26 +0800

    Update EntityBase.h

--- Classes/EntityBase.h
+++ Classes/EntityBase.h
@@ -2,34 +2,29 @@
 #define ENTITY__BASE
 
 #include "PublicDefine.h"
-class EntityAffected;
 
-class Entity:public Node{
+class Entity : public Node {
 public:
-    static unsigned long ID;
-	virtual ~Entity();
-	virtual bool init(const int&rID,const std::string &rSCsvFileName);
+    virtual ~Entity();
+    virtual bool init(const int& rId, const BasicInformation& basicInformation);
     virtual void bindSprite(Sprite* pSprite);
     virtual void doDead();
-    virtual Rect getBoundingBox()const;
-    unsigned int getmID()const;
-    virtual const Size& getContentSize()const;
+    virtual Rect getBoundingBox() const;
+    virtual const Size& getContentSize() const;
     virtual Sprite* getSprite();
+    int _iId;                // 实体ID
+    std::string _sFileName;  // 精灵文件名
+    bool _bIsDead;           // 是否已死亡
 protected:
-	virtual void initProperty(const int& rID, const std::string& rSCsvFileName);
-    virtual void deadAction(const std::string& rSDeadImageFile = "");
+    virtual void deadAction(const std::string& deadImageFile = "");
 
-    CC_SYNTHESIZE_PASS_BY_REF(int, _iID, IID);
-    CC_SYNTHESIZE_PASS_BY_REF(std::string, _sName, SName);
-    CC_SYNTHESIZE_PASS_BY_REF(std::string, _sModelName, SModelName);
-    CC_SYNTHESIZE_PASS_BY_REF(int, _iValue, IValue);
-    CC_SYNTHESIZE_PASS_BY_REF(bool, _bIsDead, IsDead);
-    CC_SYNTHESIZE_PASS_BY_REF(int, _IAnimationFrameCount, AnimationFrameCount);
-    CC_SYNTHESIZE_PASS_BY_REF(int, _iLevel, ILevel);
+    
 
 private:
-	Sprite* PSprite = nullptr;
-    unsigned long _myID;
+    Sprite* _sprite = nullptr;  // 精灵对象
 };
 
-#endif 
+
+
+
+#endif
