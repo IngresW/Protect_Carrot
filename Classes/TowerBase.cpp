c55b66eb791df0bfd9f26bd8b4e734066d00adf7
JOYLEN-SHEN <140143381+JOYLEN-SHEN@users.noreply.github.com>
2024-01-02 22:42:12 +0800

    Update TowerBase.cpp

--- Classes/TowerBase.cpp
+++ Classes/TowerBase.cpp
@@ -1,138 +1,106 @@
 #include "TowerBase.h"
-#include "CsvUtil.h"
-#include "SoundUtil.h"
-#include "EntityAffected.h"
+#include "Monster.h"
+#include "Bullet.h"
 
-
-bool TowerBase::init(const int& rId) 
+bool TowerBase::init(const int& rId, const BasicInformation basicInformation, const TowerInformation towerInformation)
 {
-    if (Entity::init(rId, TOWERCSVFILE)) 
-    {
-        initTower();
-        return true;
-    }
+	Entity::init(rId, basicInformation);
+	if (_pTowerSprite) removeChild(_pTowerSprite);
+	CC_SAFE_RELEASE_NULL(_pTowerSprite);
 
-    return false;
+	_iBulletId = towerInformation._BBullet;
+	_iAtkRange = towerInformation._dAtkRange;
+	_iCreateCost = towerInformation._iCreateCost;
+	_iUpgradeCost = towerInformation._iUpgradeCost;
+	_iValue= towerInformation._iValue;
+	_iTowerId = towerInformation._towerId;
+	_iGrade = 0;
+	_bAtkTarget = false;
+
+	_pTowerSprite = Sprite::create("tower.png");
+	if (!_pTowerSprite) {
+		return false;
+	}
+	addChild(_pTowerSprite);
+	schedule(schedule_selector(TowerBase::checkAtkTarget), 0.1f);
+	schedule(schedule_selector(TowerBase::fire), _iBulletId._iAtkSpeed);
+	return 0;
 }
 
-void TowerBase::initTower() 
+void TowerBase::upGrade()
 {
-    if (_pTowerPanel) 
-    {
-        removeChild(_pTowerPanel, true);
-        _pTowerPanel = nullptr;
-    }
+	int upGradeMoney = -_iUpgradeCost;
+	//金钱减少操作
 
-    loadTowerData();
-
-    _pGradeMark = Sprite::create();
-    _pGradeMark->setVisible(false);
-    auto aniGradeMark = Animation::create();
-    for (int i = 1; i <= 2; i++) 
-    {
-        aniGradeMark->addSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName(StringUtils::format("showupgrade0%d.png", i)));
-    }
-    aniGradeMark->setDelayPerUnit(0.5);
-    aniGradeMark->setLoops(-1);
-    _pGradeMark->setZOrder(3);
-    _pGradeMark->runAction(Animate::create(aniGradeMark));
-    _pGradeMark->setPosition(0, 50);
-    addChild(_pGradeMark);
-
-    _pTowerPanel = Sprite::createWithSpriteFrameName(CsvUtil::getInstance()->getText(_iID, en_TowerBase, TOWERCSVFILE));
-    _pTowerPanel->retain();
-    _pTowerPanel->setZOrder(1);
-    addChild(_pTowerPanel);
-    getSprite()->setZOrder(2);
+	auto upGradeCounts = this->_iGrade;
+	_iAtkRange = _iAtkRange + (_iTowerId * 20 * _iValue );
+	_iBulletId._iAtkSpeed = _iBulletId._iAtkSpeed * 0.85;
+	_iBulletId._iAtk = _iBulletId._iAtk * 1.4;
+	this->_iUpgradeCost = upGradeCounts;
 }
 
-void TowerBase::loadTowerData() 
+bool TowerBase::isInAtkRange(const Vec2& rPos)
 {
-    auto pCSVUtil = CsvUtil::getInstance();
-    _iBulletId = pCSVUtil->getInt(_iID, en_BulletId, TOWERNAME);
-    _iAtkRange = pCSVUtil->getInt(_iID, en_Range, TOWERCSVFILE);
-    _iAtkSpace = pCSVUtil->getDouble(_iID, en_Space, TOWERCSVFILE);
-    _sUpgradeCost = pCSVUtil->getText(_iID, en_UpGradeCost, TOWERCSVFILE);
-    _bIsRotation = pCSVUtil->getInt(_iID, en_IsRotation, TOWERCSVFILE);
-    _iUpgradeCount = 0;
-    _iCreateCost = pCSVUtil->getInt(_iID, en_CreateCost, TOWERCSVFILE);
-    _bIsHaveAtkTarget = false;
+	auto distance = getPosition().distance(rPos);
+	auto range = _iAtkRange + 30; // 添加30像素的额外范围，用于显示攻击范围图案
+
+	// 创建并绘制攻击范围圆形
+	auto drawNode = DrawNode::create();
+	drawNode->drawSolidCircle(getPosition(), range, 0, 30, Color4F(1.0f, 0.0f, 0.0f, 0.3f)); // 使用红色半透明颜色绘制圆形
+	addChild(drawNode);
+	return distance < _iAtkRange;
 }
 
-void TowerBase::upGrade() 
-{
-    int upGradeMoney = std::stoi(_sUpgradeCost);
-    upGradeMoney = -upGradeMoney;
-    NotificationCenter::getInstance()->postNotification("moneyChange", (Ref*)(&upGradeMoney));
-
-    auto upGradeCounts = this->getIUpgradeCount();
-    _pGradeMark->removeFromParentAndCleanup(true);
-    getSprite()->removeFromParentAndCleanup(true);
-    Entity::initProperty(this->getIID() + 1, TOWERCSVFILE);
-
-    initTower();
-    this->_iUpgradeCount = upGradeCounts;
+int TowerBase::getTowerGradeCost() {
+	return _iUpgradeCost;
 }
 
-int TowerBase::getTowerUpGradeCost() 
+void TowerBase::checkAtkTarget(float dt)
 {
-    int upGradeMoney = std::stoi(_sUpgradeCost);
-    return upGradeMoney;
+	// 过滤掉已死亡的怪物
+	MonsterVec.erase(
+		std::remove_if(MonsterVec.begin(), MonsterVec.end(), [](MonsterBase* monster) {
+			return monster->_bIsDead;
+			}),
+		MonsterVec.end()
+	);
+
+	// 检查怪物是否在攻击范围内
+	std::vector<MonsterBase*> targetsInRange;
+	for (MonsterBase* monster : MonsterVec)
+	{
+		if (isInAtkRange(monster->getPosition()))
+		{
+			targetsInRange.push_back(monster);
+		}
+	}
+
+	// 根据怪物与炮塔的距离进行排序（近到远）
+	std::sort(targetsInRange.begin(), targetsInRange.end(), [this](MonsterBase* a, MonsterBase* b) {
+		return getPosition().distance(a->getPosition()) < getPosition().distance(b->getPosition());
+		});
+
+	// 更新攻击目标
+	if (!targetsInRange.empty())
+	{
+		_pAtkTarget = targetsInRange.front();
+		_bAtkTarget = true;
+	}
+	else
+	{
+		_pAtkTarget = nullptr;
+		_bAtkTarget = false;
+	}
 }
 
-void TowerBase::showGradeMark() 
-{
-    _pGradeMark->setVisible(true);
-}
+void TowerBase::fire(float dt) {
+	if (!_pAtkTarget || _pAtkTarget->_bIsDead) return;
 
-void TowerBase::hideGradeMark() 
-{
-    _pGradeMark->setVisible(false);
-}
+	auto createBulletCF = CallFunc::create([=]() 
+		{
+		if (!_pAtkTarget) return;
+		Bullet::createBullet(_iBulletId, this, _pAtkTarget);
+		});
 
-bool TowerBase::isInAtkRange(const Vec2& rPos) 
-{
-    auto distance = getPosition().distance(rPos);
-    auto rangeSprite = Sprite::createWithSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName(StringUtils::format("range_%d", _iAtkRange) + PHOTOPOSTFIX));
-    return distance < rangeSprite->getContentSize().width / 2 + 30;
-}
-
-void TowerBase::setAtkTarget(EntityAffected* pEntityAffected) 
-{
-    CC_SAFE_RELEASE_NULL(_pAtkTarget);
-    _pAtkTarget = pEntityAffected;
-    CC_SAFE_RETAIN(_pAtkTarget);
-}
-
-void TowerBase::fireAction()
-{
-    std::string soundName = (_sName == "BStar") ? "PStar" : _sName;
-    SoundUtil::getInstance()->playEffect(StringUtils::format("Music/Towers/%s.mp3", soundName.c_str()));
-
-    auto pAnimation = Animation::create();
-    auto pSpriteFrameCache = SpriteFrameCache::getInstance();
-
-    int animationFrameCount = this->getAnimationFrameCount();
-    for (int i = 1; i <= animationFrameCount; i++)
-    {
-        std::string frameName = StringUtils::format("%s%d%s", _sModelName.c_str(), i, PHOTOPOSTFIX);
-        pAnimation->addSpriteFrame(pSpriteFrameCache->getSpriteFrameByName(frameName));
-    }
-
-    std::string firstFrameName = StringUtils::format("%s1%s", _sModelName.c_str(), PHOTOPOSTFIX);
-    pAnimation->addSpriteFrame(pSpriteFrameCache->getSpriteFrameByName(firstFrameName));
-
-    pAnimation->setDelayPerUnit(0.05f);
-    pAnimation->setLoops(1);
-
-    auto pAnimate = Animate::create(pAnimation);
-
-    getSprite()->stopAllActions();
-    getSprite()->runAction(pAnimate);
-}
-
-int TowerBase::getTowerGradeCost()
-{
-    int upGradeMoney = std::stoi(_sUpgradeCost);
-    return upGradeMoney;
+	this->runAction(Sequence::create(DelayTime::create(0.15), createBulletCF, NULL));
 }
